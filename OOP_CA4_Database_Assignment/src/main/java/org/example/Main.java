a05dbd1dcdd17aa019de83a6fb1ef1caafea030c
DorianBurak <D00268744@student.dkit.ie>
2025-02-16 19:04:46 +0000

    Added the function to add new income and expenses to database

--- OOP_CA4_Database_Assignment/src/main/java/org/example/Main.java
+++ OOP_CA4_Database_Assignment/src/main/java/org/example/Main.java
@@ -7,6 +7,7 @@ public class Main {
         Main main = new Main();
         main.start();
     }
+
     public void start() {
 
         String url = "jdbc:mysql://localhost:3306/oop_ca4_database_assignment";
@@ -16,6 +17,14 @@ public class Main {
         try (Connection conn = DriverManager.getConnection(url, user, password))
         {
             System.out.println("Successfully connected to database");
+
+            System.out.println("Expenses:");
+            printExpenses(conn);
+
+            System.out.println("Income:");
+            printIncome(conn);
+
+            System.out.println("Disconnected from database");
         }
         catch (SQLException e) {
             System.out.println("Failed to connect to database");
@@ -107,6 +116,54 @@ public class Main {
         }
         public void setDateEarned(Date dateEarned) {
             this.dateEarned = dateEarned;
+        }
+    }
+
+    private void printExpenses(Connection conn) throws SQLException {
+        String selectAll = "SELECT * FROM expenses";
+        String addExpense = "INSERT INTO oop_ca4_database_assignment.expenses Values (null,?,?,?,?)";
+        try (PreparedStatement pstmt = conn.prepareStatement(addExpense)) {
+//            pstmt.setString(1, "Leap Card");
+//            pstmt.setString(2, "Travel");
+//            pstmt.setDouble(3, 12.50);
+//            pstmt.setDate(4, new Date(2025-01-24));
+//            pstmt.executeUpdate();
+
+            Statement stmt = conn.createStatement();
+
+            ResultSet rs = stmt.executeQuery(selectAll);
+            while (rs.next()) {
+                int expenseId = rs.getInt("expenseId");
+                String title = rs.getString("title");
+                String category = rs.getString("category");
+                double amount = rs.getDouble("amount");
+                Date dateIncurred = rs.getDate("dateIncurred");
+
+                System.out.printf("ID: %d | Title: %s | Category: %s | Amount: €%.2f | Date: %s\n", expenseId, title, category, amount, dateIncurred);
+            }
+        }
+    }
+
+    private void printIncome(Connection conn) throws SQLException {
+        String selectAll = "SELECT * FROM income";
+        String addIncome = "INSERT INTO oop_ca4_database_assignment.income Values (null,?,?,?)";
+        try (PreparedStatement pstmt = conn.prepareStatement(addIncome)) {
+//            pstmt.setString(1, "Money Found");
+//            pstmt.setDouble(2, 10);
+//            pstmt.setDate(3, new Date(2025-01-11));
+//            pstmt.executeUpdate();
+
+            Statement stmt = conn.createStatement();
+
+            ResultSet rs = stmt.executeQuery(selectAll);
+            while (rs.next()) {
+                int incomeId = rs.getInt("incomeId");
+                String title = rs.getString("title");
+                double amount = rs.getDouble("amount");
+                Date dateEarned = rs.getDate("dateEarned");
+
+                System.out.printf("ID: %d | Title: %s | Amount: €%.2f | Date: %s\n", incomeId, title, amount, amount, dateEarned);
+            }
         }
     }
 }
